0818277bbbc521b52216bb3b795f1d88b71d50f9
Tony Brobston <anthonybrobston@live.com>
2024-01-03 23:05:44 -0600

    Fix axios response and fix lint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const axios = require('axios');
 const cypressSplit = require('cypress-split');
-const {determineSplit, determineSplitIndex} = require('./services/split.js');
+const { determineSplit, determineSplitIndex } = require('./services/split');
 
 const cypressSplitLocalDocker = async (on, config) => {
   if (process.env.LOCAL === 'true') {
-    const containers = await axios.get('http://localhost/v1.41/containers/json');
+    const containers = (await axios.get('http://localhost/v1.41/containers/json')).data;
     const containerId = process.env.HOSTNAME;
+    // eslint-disable-next-line no-param-reassign
     config.env.split = determineSplit(containers, containerId);
+    // eslint-disable-next-line no-param-reassign
     config.env.splitIndex = determineSplitIndex(containers, containerId);
   }
   cypressSplit(on, config);
--- test/index.test.js
+++ test/index.test.js
@@ -30,23 +30,24 @@ describe('index', () => {
     process.env.LOCAL = 'true';
     const containerId = 'c737d3401146';
     process.env.HOSTNAME = containerId;
-    const containers = [
-      {
-        Id: '1afb51b3616b7641e1a6a8ed755af02b19560a62fd8f38f28625b6ed7cab9a77',
-        Labels: {
-          'com.docker.compose.service': 'cypress',
+    axios.get.mockResolvedValue({
+      data: [
+        {
+          Id: '1afb51b3616b7641e1a6a8ed755af02b19560a62fd8f38f28625b6ed7cab9a77',
+          Labels: {
+            'com.docker.compose.service': 'cypress',
+          },
+          State: 'running',
         },
-        State: 'running',
-      },
-      {
-        Id: 'c737d3401146143189487d9f243124f2d101324d4c7a18decf2447dbf0fd5fef',
-        Labels: {
-          'com.docker.compose.service': 'cypress',
+        {
+          Id: 'c737d3401146143189487d9f243124f2d101324d4c7a18decf2447dbf0fd5fef',
+          Labels: {
+            'com.docker.compose.service': 'cypress',
+          },
+          State: 'running',
         },
-        State: 'running',
-      },
-    ];
-    axios.get.mockResolvedValue(containers);
+      ],
+    });
 
     await cypressSplitLocalDocker(on, config);
 
@@ -55,7 +56,7 @@ describe('index', () => {
       env: {
         split: 2,
         splitIndex: 1,
-      }
+      },
     };
     expect(config).toEqual(actualConfig);
     expect(cypressSplit).toHaveBeenCalledTimes(1);
